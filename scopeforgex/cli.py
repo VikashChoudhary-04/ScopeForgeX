e71254ac2d678d29b81e84e6286cb7f4e8f85ccd
Vikash Choudhary <120484001+VikashChoudhary-04@users.noreply.github.com>
2026-01-25 17:09:20 +0530

    Create cli.py

---
+++ scopeforgex/cli.py
@@ -0,0 +1,8 @@
+from scopeforgex.ui import banner, info
+from scopeforgex.dashboard import dashboard
+
+def main():
+    banner()
+    info("CLI-only Ethical Hacking Workflow Automation")
+    info("Authorized testing only ✅\n")
+    dashboard()
